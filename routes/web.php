7bfbed66a19b73a1780a858f15a13a9bb4e2b74e
Tezza1 <tshenker@gmail.com>
2016-10-30 00:26:40 +0900

    Routes, controllers, views setup

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// A route for each page. Only need index for this assignment
+Route::get("/main", "MainController@index")->name('main.index');
+
+Route::get("/lorems", "LoremController@index")->name('lorems.index');
+
+Route::get("/users","UserController@index")->name('users.index');
